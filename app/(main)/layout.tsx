2dc6cd6f6eb2c10643fd598e40ae339f109191cf
William-remake <td0311886@gmail.com>
2026-04-30 13:15:23 +0700

    add news

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,21 +1,23 @@
 import Header from "@/compnent/Header"
 import Footer from "@/compnent/Footer"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
-        <Header/>
+      <body className="flex flex-col min-h-screen antialiased">
+        <Header />
 
         <main className="flex-1 flex flex-col">
           {children}
         </main>
 
-        <Footer/>
+        <Footer />
         
-        {/* Vercel Speed Insights component */}
+        {/* Vercel Monitoring Tools */}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
